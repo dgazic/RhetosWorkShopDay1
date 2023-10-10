9955b53a62f96c2753836911ab3c284facd23de0
Dino Gazić <dgazic@omega-software.hr>
2023-10-10 16:09:12 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,44 @@
+using Rhetos;
+
+var builder = WebApplication.CreateBuilder(args);
+
+
+builder.Services.AddRhetosHost((serviceProvider, rhetosHostBuilder) => rhetosHostBuilder
+.ConfigureRhetosAppDefaults()
+.UseBuilderLogProviderFromHost(serviceProvider).ConfigureConfiguration(cfg => cfg.MapNetCoreConfiguration(builder.Configuration)))
+.AddAspNetCoreIdentityUser()
+.AddHostLogging()
+.AddDashboard()
+.AddRestApi(o =>
+{
+    o.BaseRoute = "rest";
+    o.GroupNameMapper = (conceptInfo, controller, oldName) => "v1";
+});
+
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(o => o.CustomSchemaIds(type => type.ToString())); // CustomSchemaIds allows multiple entities with the same name in different modules.
+
+var app = builder.Build();
+
+app.UseRhetosRestApi();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+    app.MapRhetosDashboard();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
